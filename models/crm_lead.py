883298cae221b7d258d41bebcf5b90d47d8762e0
jeffrepo <jeffer.leo93@gmail.com>
2023-04-25 01:25:41 -0600

    solo crear lead

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -15,20 +15,21 @@ class Lead(models.Model):
         # username = 'leonardo@contenedoresmas.com'
         # password = 'Contenedoresmas23'
         for record in self:
-            print ("URLL", url)
-            common = xmlrpc.client.ServerProxy(url +'/common')
-            uid = common.login(db, username, password)
-            print ("UIDDD", uid)
-            models = xmlrpc.client.ServerProxy(url +'/object')
-            prodid = models.execute_kw(db, uid, password, 'crm.lead', 'create', [{
-                'contact_name': record.contact_name,
-                'name': record.name,
-                'type': record.type,
-                'email_from': record.email_from,
-                'mobile': record.mobile,
-                'city': record.city,
-                'description': record.description.replace('\n', '<br>') if record.description else "",
+            if record.type == "lead":
+                print ("URLL", url)
+                common = xmlrpc.client.ServerProxy(url +'/common')
+                uid = common.login(db, username, password)
+                print ("UIDDD", uid)
+                models = xmlrpc.client.ServerProxy(url +'/object')
+                prodid = models.execute_kw(db, uid, password, 'crm.lead', 'create', [{
+                    'contact_name': record.contact_name,
+                    'name': record.name,
+                    'type': record.type,
+                    'email_from': record.email_from,
+                    'mobile': record.mobile,
+                    'city': record.city,
+                    'description': record.description.replace('\n', '<br>') if record.description else "",
 
-              }])
-            print ("prodid",prodid)
+                  }])
+                print ("prodid",prodid)
         return True
