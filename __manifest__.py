4b512ffd431af3a92b750d0c91536cb420f47dab
jeffrepo <jeffer.leo93@gmail.com>
2023-04-28 13:40:06 -0600

    Campo nuevo

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,8 @@
     'author': 'JS',
     'depends': ['base','base_automation','crm'],
     'data': [
-        'data/base_automation_data.xml',
+        # 'data/base_automation_data.xml',
+        'views/crm_lead_views.xml',
     ],
 
     'installable': True,
--- models/crm_lead.py
+++ models/crm_lead.py
@@ -9,27 +9,26 @@ import xmlrpc.client
 class Lead(models.Model):
     _inherit = "crm.lead"
 
-    def crear_oportunidad(self, url, db, username, password):
-        # url = "https://avanguardiatech-contenedoresmas-pruebas-7433491.dev.odoo.com/xmlrpc"
-        # db = 'avanguardiatech-contenedoresmas-pruebas-7433491'
-        # username = 'leonardo@contenedoresmas.com'
-        # password = 'Contenedoresmas23'
-        for record in self:
-            if record.type == "lead":
-                print ("URLL", url)
-                common = xmlrpc.client.ServerProxy(url +'/common')
-                uid = common.login(db, username, password)
-                print ("UIDDD", uid)
-                models = xmlrpc.client.ServerProxy(url +'/object')
-                prodid = models.execute_kw(db, uid, password, 'crm.lead', 'create', [{
-                    'contact_name': record.contact_name,
-                    'name': record.name,
-                    'type': record.type,
-                    'email_from': record.email_from,
-                    'mobile': record.mobile,
-                    'city': record.city,
-                    'description': record.description.replace('\n', '<br>') if record.description else "",
+    medida_contenedor = fields.Char('Medidas de contenedor')
 
-                  }])
-                print ("prodid",prodid)
-        return True
+    # def crear_oportunidad(self, url, db, username, password):
+    #
+    #     for record in self:
+    #         if record.type == "lead":
+    #             print ("URLL", url)
+    #             common = xmlrpc.client.ServerProxy(url +'/common')
+    #             uid = common.login(db, username, password)
+    #             print ("UIDDD", uid)
+    #             models = xmlrpc.client.ServerProxy(url +'/object')
+    #             prodid = models.execute_kw(db, uid, password, 'crm.lead', 'create', [{
+    #                 'contact_name': record.contact_name,
+    #                 'name': record.name,
+    #                 'type': record.type,
+    #                 'email_from': record.email_from,
+    #                 'mobile': record.mobile,
+    #                 'city': record.city,
+    #                 'description': record.description.replace('\n', '<br>') if record.description else "",
+    #
+    #               }])
+    #             print ("prodid",prodid)
+    #     return True
