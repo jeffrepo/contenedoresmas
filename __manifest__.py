d747f6d43cbaa6fcbc7ba6235600b5413b39c8e8
angel <angelmendez00512@gmail.com>
2024-07-31 12:09:15 -0600

    Migracion para la version 17

--- __manifest__.py
+++ __manifest__.py
@@ -12,12 +12,13 @@
     'depends': ['base','base_automation','crm','product','account'],
     'data': [
         # 'data/base_automation_data.xml',
+        'wizard/inventario_ventas_views.xml',
         'views/crm_lead_views.xml',
         'views/reporte_invoice.xml',
         'views/account_move_views.xml',
         'views/product_template_views.xml',
         'report/reporte_inventario_ventas_views.xml',
-        'wizard/inventario_ventas_views.xml',
+        
     ],
 
     'installable': True,
